45b3da86b3eddd660360cc7c35f44f3cc25a5ec5
16Q45T <etancelin.edouard@gmail.com>
2017-10-13 19:56:19 +0200

    Implement The navbar with a very nice smothScrool Transition.
    To make the transition on scrool Cool
    Choice have been made to make the transition with javascript instead of css use this tiny tiny Library https://github.com/alicelieutier/smoothScroll.
    For 2 reasons
    	# React Component are JS component using a js Library from NPM is simple in this case.
    	# All of CSS should have write to get the same result
    	# The library is very simple to Use.

--- app/javascript/ccc/components/app.jsx
+++ app/javascript/ccc/components/app.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import smoothscroll from 'smoothscroll';
+import banner from 'fight.jpg'
 
+// Helping Functions
+const bannerStyle = (backgroundImage) => {
+  return ({
+      backgroundImage: `linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%), url('${backgroundImage}')`
+  })
+}
+
+// Scroll to the Character Session
+const onPLayClick = () => {
+  const destination = document.querySelector('.character');
+  smoothscroll(destination, 1300)
+}
+
+// Components
 const App = (props) => {
   return (
-    <div className="messaging-wrapper">
-      <div className="logo-container">
-        <img className="messaging-logo" src="assets/images/logo.svg" />
-        coucvousdqdkqbjl
+    <div >
+      <div className="banner fx-c-sa-c" style={bannerStyle(banner)}>
+        <div className="banner-content">
+          <h1>Captain Contrat Fight</h1>
+          <p>made with <i className="em em-heart"></i> by Doudou </p>
+        </div>
+        <a className="btn btn-primary btn-lg"
+            onClick={onPLayClick}>
+            Play now</a>
+      </div>
+      <div className="character h-100vh">
+        Coucou Ici il va y avoir des personnages bientôt ;-)
       </div>
     </div>
   );
